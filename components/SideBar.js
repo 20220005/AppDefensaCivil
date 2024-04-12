f9803d65db629223bf1b5f9948762e2c40bb21bb
20220005 <20220005@itla.edu.do>
2024-04-11 20:38:39 -0400

    Dando estilos al navbar

--- components/SideBar.js
+++ components/SideBar.js
@@ -1,49 +1,65 @@
-// Sidebar.js
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Image, Pressable, FlatList } from "react-native";
+import icono from "../assets/icon-defensa-civil.png";
 
 const Sidebar = ({ navigation }) => {
+  const menuItems = [
+    { key: "home", title: "HOME" },
+    { key: "miembros", title: "MIEMBROS" },
+    { key: "servicios", title: "SERVICIOS" },
+    { key: "noticias", title: "NOTICIAS" },
+    { key: "videos", title: "VIDEOS" },
+    { key: "albergues", title: "ALBERGUES" }
+  ];
+  const menuItemsLogin= [
+    { key: "home", title: "HOME" },
+    { key: "miembros", title: "MIEMBROS" },
+    { key: "servicios", title: "SERVICIOS" },
+    { key: "noticias", title: "NOTICIAS" },
+    { key: "videos", title: "VIDEOS" },
+    { key: "albergues", title: "ALBERGUES" }
+  ];
+
+  const renderItem = ({ item }) => (
+    <View style={styles.mid}>
+    <Pressable
+      style={styles.button}
+      onPress={() => navigation.navigate(item.key)}
+    >
+      <Text style={styles.text}>{item.title}</Text>
+    </Pressable>
+    </View>
+  );
+
   return (
     <View style={styles.sidebar}>
       <View style={styles.top}>
-        <Text>Menu</Text>
+        <Image source={icono} style={styles.icono} />
       </View>
-        <View style={styles.mid}>
-          <Button
-          title="Home"
-          onPress={() => navigation.navigate('Home')}
-          />
-          <Button
-          title="Miembros"
-          onPress={() => navigation.navigate('Miembros')}
-          />
-          <Button
-          title="Servicios"
-          onPress={() => navigation.navigate('Servicios')}
-          />
-          <Button
-          title="Noticias"
-          onPress={() => navigation.navigate('Noticias')}
-          />
-          <Button
-          title="Videos"
-          onPress={() => navigation.navigate('Videos')}
-          />
-          <Button
-          title="Albergues"
-          onPress={() => navigation.navigate('Albergues')}
-          />
+      <View style={styles.mid}>
+        <FlatList
+          data={menuItems}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.key}
+      
+        />
+      </View>
+      <View style={styles.bottom}>
+      <Pressable
+          style={{ ...styles.button, backgroundColor: "#0a509e" }}
+          onPress={() => navigation.navigate("Registrar")}
+        >
+          <Text style={styles.text}>REGISTRARSE</Text>
+        </Pressable>
+        
+        <Pressable
+          style={{ ...styles.button, backgroundColor: "#0a509e" }}
+          onPress={() => navigation.navigate("Login")}
+        >
+          <Text style={styles.text}>INICIAR SESIÓN</Text>
+        </Pressable>
 
-        </View>
-        <View style={styles.bottom}>
-        <Button
-        title="Registrarse"
-        onPress={() => navigation.navigate('Registrar')}
-      />
-         <Button
-        title="Iniciar Sesión"
-        onPress={() => navigation.navigate('Login')}
-      />
+       
       </View>
     </View>
   );
@@ -52,28 +68,49 @@ const Sidebar = ({ navigation }) => {
 const styles = StyleSheet.create({
   sidebar: {
     flex: 1,
-    backgroundColor: 'white',
-    padding: 20,
-   
+    backgroundColor: "white",
   },
   top: {
-    height: 60,
-    alignItems: 'center',
-    justifyContent: 'center',
+    height: "25%",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#0a509e",
+    width: "100%",
+  },
+  icono: {
+    width: 100,
+    height: 100,
+    borderRadius: 100 / 2,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 10,
   },
   mid: {
     flex: 1,
     padding: 10,
-    gap:10
+    gap: 5,
+   
+  },
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "#fb7405",
+    borderRadius: 5,
+  },
+  text: {
+    color: "white",
+    fontWeight: "bold",
   },
   bottom: {
-    position: 'absolute',
+    height: "20%",
+    position: "absolute",
     bottom: 0,
     left: 0,
     right: 0,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     padding: 10,
-    gap:10
+    gap: 10,
   },
 });
 
