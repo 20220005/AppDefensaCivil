9ac071d6251135344ad4689615b1d5f898be96d3
Omar Sanchez <omisp21@gmail.com>
2024-04-12 21:00:55 -0400

    Mapa Arreglado

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import NoticiasEspecificas from "./components/NoticiasEspecificas";
 import ExtraScreen from "./screens/ExtraScreen";
 import MededidasPreventivas from './components/MedidasPreventivas';
 import RecuperarContraseña from './components/RecuperarContraseña';
+import MapaInteractivo from './components/MapaInteractivo';
 
 const Drawer = createDrawerNavigator();
 
@@ -33,7 +34,7 @@ const App = () => {
        <Drawer.Screen name='Extras' component={ExtraScreen} />
        <Drawer.Screen name='MedidasPreventivas' component={MededidasPreventivas} />
        <Drawer.Screen name='Recuperar' component={RecuperarContraseña} />
-
+       <Drawer.Screen name='MapaAlbergues' component={MapaInteractivo} />
 
        
       </Drawer.Navigator>
